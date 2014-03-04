f7e538afb5fd2c0e2e66a9fc46ea2215039656cc
Schroedingers-Hat <explodymatt@gmail.com>
2014-03-05 02:38:30 +1000

    Added new file using numpy
    Same method as old file, just introduced numpy. Much slower because all
    of the overhead of C calls with no benefit as yet, also somewhat tidier.

---
+++ lalg_gravity_sim.py
@@ -0,0 +1,80 @@
+import time
+import tkinter as tk
+import math
+import numpy
+## Keeping the namespace reasonably tidy for now.
+## probably wise to do from numpy import * at some point.
+from numpy import matrix
+from random import random
+
+## Celestial body with numpy matrices position X, velocity V, and mass m.
+class Body:
+    def __init__(self, X, V, m, canvas):
+        self.X = matrix(X)
+        self.V = matrix(V)
+        self.tempForce = matrix([0.,0.])
+        self.mass = m
+        self.canvas = canvas
+        ## Let the point take care of its oval, that way we can update positions rather than
+        ## Recreate them all. NB: Tk is terrible for this type of thing.
+        self.oval = canvas.create_oval(self.X.A1[0] - 1, self.X.A1[1] - 1,
+                         self.X.A1[0] + 1, self.X.A1[1] + 1,
+                        fill = 'white',
+                        outline = 'white')
+    def redraw(self):
+        self.canvas.coords(self.oval,self.X.A1[0] - 1, self.X.A1[1] - 1,
+                         self.X.A1[0] + 1, self.X.A1[1] + 1)
+
+## Field of celestial bodies, main integration and such should happen here.
+class Field:
+    G = 6.67E-11
+    def __init__(self, numBodies, canvas):
+        self.bodArr = [None] * numBodies
+        for i in range(len(self.bodArr)):
+            x = random() - 0.5
+            y = random() - 0.5
+            self.bodArr[i] = Body(matrix([x,y])*400,matrix([0.,0.]),random()*1E8, canvas)
+        ## Don't forget to make EVERYTHING a float, or you're going to have a bad time.
+
+        ## Large central body.
+        self.bodArr[0].X = matrix([0.,0.])
+        self.bodArr[0].V = matrix([0.,0.])
+        self.bodArr[0].mass = 1.1 * 1E12
+
+    ## Integrator, still euler with the loop in the python. Now with added overhead!
+    def update(self):
+        for body in self.bodArr:
+            body.tempForce = matrix([0.,0.])
+            for other in self.bodArr:
+                if not (body == other):
+                    ## GMm/r**2 for each other body
+                    body.tempForce += (Field.G * body.mass * other.mass) * (numpy.linalg.norm(body.X - other.X)**-2 * -(body.X - other.X))
+        for body in self.bodArr:
+            ## dV/dt = F/m
+            body.V += body.tempForce / body.mass
+            ## dX/dt = V
+            body.X += body.V
+
+## Draw and update function.
+class Draw():
+    def __init__(self, master,w = 800,h = 800):
+        self.width = w
+        self.height = h
+        self.canvas = tk.Canvas(master, width = self.width, height = self.height, background = 'black')
+        ## Can't for the life of me figure out what a 'unit' is. Something a bit less than a pixel.
+        ## This at least puts the origin in the middle somewhere.
+        self.canvas.xview_scroll(w*2, "units")
+        self.canvas.yview_scroll(h*2, "units")
+        self.canvas.pack()
+        self.field = Field(20, self.canvas)
+    def drawFrame(self):
+        self.field.update()
+        for body in self.field.bodArr:
+            body.redraw()
+        self.canvas.update()
+        time.sleep(1/30)
+
+root = tk.Tk()
+d = Draw(root)
+while True:
+    d.drawFrame()
