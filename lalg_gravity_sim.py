acfa7f980c1c8ed72adbc6dbfdc02f903192fa2e
alexstasse <turks.jpg@gmail.com>
2014-03-05 12:00:54 +1000

    Prettifying
    Points now scale in size relative to mass, made distribution polar and
    with initial velocities.  Decreased canvas size (for my laptop screen
    :D) and centered sun in canvas (seems canvas is 2000 units across?)

--- lalg_gravity_sim.py
+++ lalg_gravity_sim.py
@@ -15,15 +15,16 @@ class Body:
         self.tempForce = matrix([0.,0.])
         self.mass = m
         self.canvas = canvas
+        self.r = m**(1/3) / 200
         ## Let the point take care of its oval, that way we can update positions rather than
         ## Recreate them all. NB: Tk is terrible for this type of thing.
-        self.oval = canvas.create_oval(self.X.A1[0] - 1, self.X.A1[1] - 1,
-                         self.X.A1[0] + 1, self.X.A1[1] + 1,
+        self.oval = canvas.create_oval(self.X.A1[0] - self.r, self.X.A1[1] - self.r,
+                         self.X.A1[0] + self.r, self.X.A1[1] + self.r,
                         fill = 'white',
                         outline = 'white')
     def redraw(self):
-        self.canvas.coords(self.oval,self.X.A1[0] - 1, self.X.A1[1] - 1,
-                         self.X.A1[0] + 1, self.X.A1[1] + 1)
+        self.canvas.coords(self.oval,self.X.A1[0] - self.r, self.X.A1[1] - self.r,
+                         self.X.A1[0] + self.r, self.X.A1[1] + self.r)
 
 ## Field of celestial bodies, main integration and such should happen here.
 class Field:
@@ -31,15 +32,22 @@ class Field:
     def __init__(self, numBodies, canvas):
         self.bodArr = [None] * numBodies
         for i in range(len(self.bodArr)):
-            x = random() - 0.5
-            y = random() - 0.5
-            self.bodArr[i] = Body(matrix([x,y])*400,matrix([0.,0.]),random()*1E8, canvas)
+            #x = random() - 0.5
+            #y = random() - 0.5
+            r = random()*150
+            a = random() * 2 * math.pi
+            x = r * math.cos(a)
+            y = r * math.sin(a)
+            m = random()*1E8
+            xv = -y/r * (Field.G*(1E12+m)/r)**.5 * 6 #this shouldn't need to be multiplied by 5, something wacky is going on
+            yv =  x/r * (Field.G*(1E12+m)/r)**.5 * 6
+            self.bodArr[i] = Body(matrix([x,y]),matrix([xv,yv]),m, canvas)
         ## Don't forget to make EVERYTHING a float, or you're going to have a bad time.
 
         ## Large central body.
         self.bodArr[0].X = matrix([0.,0.])
         self.bodArr[0].V = matrix([0.,0.])
-        self.bodArr[0].mass = 1.1 * 1E12
+        self.bodArr[0].mass = float(1E12)
 
     ## Integrator, still euler with the loop in the python. Now with added overhead!
     def update(self):
@@ -57,14 +65,14 @@ class Field:
 
 ## Draw and update function.
 class Draw():
-    def __init__(self, master,w = 800,h = 800):
+    def __init__(self, master,w = 600,h = 600):
         self.width = w
         self.height = h
         self.canvas = tk.Canvas(master, width = self.width, height = self.height, background = 'black')
         ## Can't for the life of me figure out what a 'unit' is. Something a bit less than a pixel.
         ## This at least puts the origin in the middle somewhere.
-        self.canvas.xview_scroll(w*2, "units")
-        self.canvas.yview_scroll(h*2, "units")
+        self.canvas.xview_scroll(1000, "units")
+        self.canvas.yview_scroll(1000, "units")
         self.canvas.pack()
         self.field = Field(20, self.canvas)
     def drawFrame(self):
